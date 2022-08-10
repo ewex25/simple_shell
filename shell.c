5abdae6cd892dfac4420dec4e76442329699e166
phatboislym <phatboilsym@protonmail.com>
2022-08-11 00:11:05 +0100

    new tack

--- shell.c
+++ shell.c
@@ -1,57 +1,99 @@
 #include "shell.h"
-
 /**
- *main - Displays a prompt and waits for the user to enter a command
- *Return: 0 on success
+ * sighand - function to handle ctrl+c
+ * @signum: signal number
  */
-int main(int argc, char *argv[])
+void sighand(int signum)
 {
-	char **tokens;
-	char *line;
-	int status;
-	struct stat st;
+	(void)signum;
+	write(1, "\n$ ", 3);
+}
+/**
+ * main - simple shell program
+ * Return: 0 always success
+ */
+int main(void)
+{
+	/*unsigned int pid, ppid;*/
+	ssize_t read = 0;
+	char *buff = NULL, **arr;
+	size_t size = 0;
+	int result, tok_size = 0, len = 0, line_counter = 0;
+	int ex_flag = 0;
 
-	signal(SIGINT, ctrl_c);
-	if (fstat(0, &st) == -1)
-		perror("fstat");
-	status = 1;
-	do {
-/*print prompt if command is not piped*/
-		print_prompt();
-/*read input from stdin*/
-		line = read_line();
-		if (_strcmp(line, "\n") == 0)
+	while (1)
+	{
+		size = 0;
+		buff = NULL;
+
+		if (isatty(0))
+			write(STDIN_FILENO, "$ ", 2);
+		signal(SIGINT, sighand);
+		read = getline(&buff, &size, stdin);
+		line_counter++;
+		if (read == -1)
 		{
-			tokens = NULL;
-			free(line);
+			if (isatty(0))
+				write(STDIN_FILENO, "\n", 1);
+			free(buff);
+			return (0);
+		}
+		if (read == 0)
+		{
+			if (isatty(0))
+			{
+				write(STDIN_FILENO, "\n", 1);
+				continue;
+			}
+		}
+		if (buff && buff[0] != '\n')
+		{
+			/* count length of buffer input */
+			len = _strlen(buff);
+			if (buff[len - 1] == '\n')
+				buff[len - 1] = '\0';
+
+			/* count number of tokens in buffer */
+			tok_size = toksize(buff);
+			if (tok_size == -1)
+				break;
+			if (tok_size == 0)
+				continue;
+			/* put tokens inside array */
+			arr = tokenize(buff);
+			/* checks if 'env', 'exit', or '.' is entered */
+			result = str_comp(arr, tok_size);
+			if (result == 0)
+			{
+				free(arr);
+				free(buff);
+				exit(ex_flag);
+			}
+			ex_flag = 0;
+			if (result == 2)
+			{
+				free(arr);
+				free(buff);
+				continue;
+			}
+			else if (result == 1)
+			{
+				print_env();
+				free(arr);
+				free(buff);
+				continue;
+			}
+			/* send input to path function to check */
+			/*if it exists, permissions, and if it can execute */
+			ex_flag = path(arr, line_counter);
+		}
+		if (buff && buff[0] == '\n')
+		{
+			free(buff);
 			continue;
 		}
-		else
-		{
-			printf("%i", argc);
-		}
-		get_history(line);
-/*split the line into tokens*/
-		tokens = _strtotokens(line);
-		if (tokens[0] == NULL)
-		{
-			free(tokens);
-			free(line);
-			continue;
-		}
-/*handle exit invokation*/
-		if (_strcmp(tokens[0], "exit") == 0)
-		{
-			shell_exit(tokens, line);
-		}
-		else
-		{
-/*execute commands*/
-			status = _execute(tokens, line, argv[0]);
-		}
-/*free memory*/
-		free(line);
-		free(tokens);
-	} while (status == 1);
-	return (status);
+		free(buff);
+		free(arr);
+	}
+	return (0);
 }
--- shell.h
+++ shell.h
@@ -1,75 +1,47 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef _SHELL_H_
+#define _SHELL_H_
+#define _GNU_SOURCE
 
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <signal.h>
-#include <fcntl.h>
+/**
+ * struct environ_list - singly linked list
+ * @str: string - (malloc'ed string)
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct environ_list
+{
+	char *str;
+	struct environ_list *next;
+} env_list;
 
-#define PATH_MAX 1024
+void sighand(int signum);
 extern char **environ;
+void print_env(void);
+void error(char *str, int line_counter, int flag);
+int _strlen(char *s);
+void rev_string(char *s);
+char *_itoa(int n);
+unsigned int get_pid(void);
+unsigned int get_ppid(void);
+char *stringdup(const char *str);
+int toksize(char *str);
+char **tokenize(char *str);
+int few(pid_t c_pid, char **arr, int line_counter);
+int str_comp(char **arr, int size);
+char *_getenv(const char *name);
+int _which(char *file);
+char *str_concat(char *s1, char *s2, int flag);
+int path(char **arr, int line_counter);
 
-int main(int argc, char *argv[]);
 
-/*used in _strings.c file*/
-char *_strcat(char *dest, char *src);
-int _strcmp(char *s1, char *s2);
-char *_strcpy(char *dest, char *src);
-char *_strdup(char *str);
-int _strlen(const char *s);
-
-/*used in envset_unset.c*/
-int shell_environ(void);
-int _setenv(char *var_name, char *var_value);
-char *var_build(char *var_name, char *var_value);
-int _unsetenv(char *var_name);
-
-/*used in cd_handler.c file*/
-char *_getcwd(void);
-int cd_home(void);
-int cd_prev(void);
-int _cd(char **args);
-
-/*used in ctrl_exit.c file*/
-void print_prompt(void);
-char *ignore_space(char *str);
-void ctrl_c(int signum);
-void shell_exit(char **args, char *line);
-
-/*used in execute.c file*/
-unsigned int _occurence(char *s);
-char **_strtotokens(char *str);
-int check_file_status(char *filename);
-int _execute(char **tokens, char *line, char *args);
-
-/*used in read_line.c file*/
-char *read_line2(void);
-void free_args(char **args);
-char *read_line(void);
-
-/*used in _realloc.c file*/
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
-/*used in builtin_parser.c file*/
-int builtin_parser(char **tokens);
-
-/*used in path_handler.c file*/
-int get_path(char **args);
-char *get_env(char *path);
-char *cmd_build(char *token, char *dir_value);
-
-/*used in print_char.c file*/
-int _putchar(char c);
-void _print(char *str);
-
-int shell_help(char **args);
-/*history*/
-int get_history(char *input);
-int display_history(void);
-
-#endif /*SHELL-H*/
+#endif /* _SHELL_H_ */
